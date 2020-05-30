087efee515caf61641af4dc90b92dbf89cc4e3c2
nargok <nargok@gmail.com>
2020-05-30 20:01:47 +0900

    Initial Commit

---
+++ ARShooting/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  ARShooting
+//
+//  Created by 後閑諒一 on 2020/05/30.
+//  Copyright © 2020 nargok. All rights reserved.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+    var body: some View {
+        return ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
